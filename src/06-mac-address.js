c665ff9831f302df22e0166897ded4ee207ec5d9
Alina Balysh <lina.balysh@gmail.com>
2021-04-12 14:55:23 +0300

    feat: implement 6th task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -5,15 +5,22 @@
  * Your task is to check by given string inputString
  * whether it's a MAC-48 address or not.
  *
- * @param {Number} inputString
+ * @param {Number} n
  * @return {Boolean}
  *
  * @example
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const digitsArr = n.split('-');
+  if (digitsArr.length < 2 || digitsArr[0].length > 2) return false;
+  const reg = /[0-9A-F]{2}/g;
+  for (let i = 0; i < digitsArr.length; i++) {
+    if (!reg.test(digitsArr[i])) return false;
+    reg.lastIndex = 0;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
