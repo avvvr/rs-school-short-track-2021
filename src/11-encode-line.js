f25b4dd37807c59224b8d90cea0bbc2158fb437f
Alina Balysh <lina.balysh@gmail.com>
2021-04-13 00:03:17 +0300

    feat: implement 11th task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const letters = str.split('');
+  const result = [];
+
+  let currentChar = letters[0];
+  let counter = 1;
+  for (let i = 1; i < letters.length; i++) {
+    if (letters[i] === currentChar) {
+      counter++;
+    } else {
+      result.push(counter === 1 ? '' : counter);
+      result.push(currentChar);
+      counter = 1;
+      currentChar = letters[i];
+    }
+  }
+
+  result.push(counter === 1 ? '' : counter);
+  result.push(currentChar);
+
+  return result.join('');
 }
 
 module.exports = encodeLine;
