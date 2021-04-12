37b12575224fd28849ed4cf1d37f7f8ce54fa5de
Alina Balysh <lina.balysh@gmail.com>
2021-04-12 21:45:11 +0300

    feat: implement 12th task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
-}
+function getDNSStats(domains) {
+  const resultsMap = new Map();
+  let currentWord = '';
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i];
+    for (let j = domain.length - 1; j >= 0; j--) {
+      if (domain[j] === '.') {
+        if (resultsMap.has(currentWord)) {
+          resultsMap.set(currentWord, resultsMap.get(currentWord) + 1);
+        } else {
+          resultsMap.set(currentWord, 1);
+        }
+      }
+      currentWord += domain[j];
+    }
+    if (resultsMap.has(currentWord)) {
+      resultsMap.set(currentWord, resultsMap.get(currentWord) + 1);
+    } else {
+      resultsMap.set(currentWord, 1);
+    }
+    currentWord = '';
+  }
+  const dictionary = new Map();
+  resultsMap.forEach((value, key) => {
+    const words = key.split('.');
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].split('').reverse().join('');
+    }
 
+    dictionary.set(`.${words.join('.')}`, resultsMap.get(key));
+  });
+  return Object.fromEntries(dictionary);
+}
 module.exports = getDNSStats;
