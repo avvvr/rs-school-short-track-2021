0308013d90cd00b1e678a3cf21e9627c21458dc7
Alina Balysh <lina.balysh@gmail.com>
2021-04-12 13:30:55 +0300

    feat: implement 4th task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,74 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const resultMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    resultMatrix.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      resultMatrix[i].push(0);
+    }
+  }
+
+  const maxi = matrix.length;
+  for (let i = 0; i < matrix.length; i++) {
+    const maxj = matrix.length - 1;
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) {
+        if (i === 0 && j === 0) {
+          resultMatrix[i][j + 1]++;
+          resultMatrix[i + 1][j]++;
+          resultMatrix[i + 1][j + 1]++;
+        } else if (i === 0 && j > 0 && j < maxj) {
+          resultMatrix[i][j - 1]++;
+          resultMatrix[i][j + 1]++;
+          resultMatrix[i + 1][j - 1]++;
+          resultMatrix[i + 1][j]++;
+          resultMatrix[i + 1][j + 1]++;
+        } else if (i === 0 && j === maxj) {
+          resultMatrix[i][j - 1]++;
+          resultMatrix[i + 1][j - 1]++;
+          resultMatrix[i + 1][j]++;
+        } else if (i > 0 && i < maxi && j === 0) {
+          resultMatrix[i - 1][j]++;
+          resultMatrix[i + 1][j]++;
+          resultMatrix[i - 1][j + 1]++;
+          resultMatrix[i][j + 1]++;
+          resultMatrix[i + 1][j + 1]++;
+        } else if (i > 0 && i < maxi && j > 0 && j < maxj) {
+          resultMatrix[i - 1][j - 1]++;
+          resultMatrix[i][j - 1]++;
+          resultMatrix[i + 1][j - 1]++;
+          resultMatrix[i - 1][j]++;
+          resultMatrix[i + 1][j]++;
+          resultMatrix[i - 1][j + 1]++;
+          resultMatrix[i][j + 1]++;
+          resultMatrix[i + 1][j + 1]++;
+        } else if (i > 0 && i < maxi && j === maxj) {
+          resultMatrix[i - 1][j - 1]++;
+          resultMatrix[i][j - 1]++;
+          resultMatrix[i + 1][j - 1]++;
+          resultMatrix[i - 1][j]++;
+          resultMatrix[i + 1][j]++;
+        } else if (i === maxi && j === 0) {
+          resultMatrix[i - 1][j]++;
+          resultMatrix[i - 1][j + 1]++;
+          resultMatrix[i][j + 1]++;
+        } else if (i === maxi && j > 0 && j < maxj) {
+          resultMatrix[i - 1][j - 1]++;
+          resultMatrix[i][j - 1]++;
+          resultMatrix[i - 1][j]++;
+          resultMatrix[i - 1][j + 1]++;
+          resultMatrix[i][j + 1]++;
+        } else if (i === maxi && j === maxj) {
+          resultMatrix[i - 1][j - 1]++;
+          resultMatrix[i][j - 1]++;
+          resultMatrix[i - 1][j]++;
+        }
+      }
+    }
+  }
+  return resultMatrix;
 }
 
 module.exports = minesweeper;
